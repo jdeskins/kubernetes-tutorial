1042422a2be2bb2de601ad1feae488a719947e1e
Jeff Deskins <Jeff.Deskins@lifeway.com>
2017-02-03 15:44:31 -0600

    Pass params to script; Added kubectl commands

--- examples/blue-green/blue-green-deploy.sh
+++ examples/blue-green/blue-green-deploy.sh
@@ -1,31 +1,63 @@
 #!/bin/bash -e
 
-#kubectl get svc hello-app -o json
 
-# TODO: Change to version
-CURRENT_VERSION=$(kubectl get svc hello-app -o 'jsonpath={.spec.selector.version}')
+usage="Usage: $(basename "$0") -a <APP_NAME> -f <DEPLOY_FILE> -v <NEW_VERSION>
+ Script for blue-green deployment.
 
-echo "Current version: $CURRENT_VERSION"
+Available options are:
+    -a  Application name
+    -f  Relative path to deploy file (ex: deployments/v2/service.yaml)
+    -h  Show this help text
+    -v  New version to point existing service
+"
 
-# TODO: Update svc to use new version in label selector
+# Capture args
+while getopts ":a:f:h:v:" opt; do
+  case $opt in
+    a) APP_NAME="$OPTARG"
+    ;;
+    f) DEPLOY_FILE="$OPTARG"
+    ;;
+    h) echo "$usage"
+    exit
+    ;;
+    v) NEW_VERSION="$OPTARG"
+    ;;
+    \?) echo "$usage" >&2
+    exit 1
+    ;;
+  esac
+done
+
+# Verify args
+if (( $# != 6 ))
+then
+  echo "$usage"
+  exit 1
+fi
+
+echo "Updating service for app: $APP_NAME, version: $NEW_VERSION, file: $DEPLOY_FILE"
+
+
+# Capture current version service is using
+OLD_VERSION=$(kubectl get svc ${APP_NAME} -o 'jsonpath={.spec.selector.version}')
+echo "Old version: $OLD_VERSION"
+
+
+# TODO: Verify new version has pods ready
+RESULT=$(kubectl get pods -l app=$APP_NAME -l version=$NEW_VERSION -o 'jsonpath={.items[*].status.phase}')
+echo "$RESULT"
+
+## TODO: Update svc to use new version in label selector
+#kubectl apply -f $DEPLOY_FILE
 
 
 # Find the replica set for old version
-# kubectl get rs
+REPLICA_SET=$(kubectl get rs -l app=$APP_NAME -l version=$OLD_VERSION -o 'jsonpath={.items[*].metadata.name}')
+echo "Orphaned Replica Set to delete: $REPLICA_SET"
 
-# COMMAND HISTORY
-#  590  kubectl apply -f deployments/helloworld/helloworld-v1.yaml
-#  591  kubectl apply -f deployments/helloworld/helloworld-v2.yaml
-#  592  kubectl apply -f deployments/helloworld/helloworld-service.yaml
-#  593  kubectl apply -f deployments/helloworld/helloworld-service-v1.yaml
-#  594  kubectl apply -f deployments/helloworld/helloworld-service-v1.yaml
-#  595  kubectl get rs --show-labels
-#  596  kubectl get svc --show-labels
-#  597  kubectl get svc hello-app -o yaml
-#  598  kubectl get rs --show-labels
-#  599  kubectl delete service,deployment hello-app
-#  600  kubectl get rs --show-labels
-#  601  kubectl delete rs hello-app-3643595958
 
-# TO SEE VERSION INFO FROM WEB
-# while true; do curl http://192.168.99.100:32508/version; sleep .5; done
+## TODO: Delete old replica set
+#kubectl delete rs $REPLICA_SET
+
+echo "FINISHED"
